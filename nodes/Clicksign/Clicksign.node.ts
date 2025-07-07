6e919443f5b561c4ae094bc6814d4d181500d409
Andrew Oliveira <andrew.oliveira@clicksign.com>
2025-07-07 12:43:16 -0300

    (+) Create first node actions for the clicksign N8N custom node

---
+++ nodes/Clicksign/Clicksign.node.ts
@@ -0,0 +1,52 @@
+import type { INodeType, INodeTypeDescription } from 'n8n-workflow';
+import { NodeConnectionType } from 'n8n-workflow';
+import { clicksignOperations, clicksignFields } from './ClicksignDescription';
+
+export class Clicksign implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Clicksign',
+		name: 'clicksign',
+		group: ['transform'],
+		version: 1,
+		description: 'Interact with Clicksign API',
+		subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
+		defaults: {
+			name: 'Clicksign',
+		},
+		inputs: [NodeConnectionType.Main],
+		outputs: [NodeConnectionType.Main],
+		credentials: [
+			{
+				name: 'clicksignApi',
+				required: true,
+			},
+		],
+		requestDefaults: {
+			baseURL: '=https://{{$credentials.clicksign_environment}}.clicksign.com/api/v3',
+      url: '',
+			headers: {
+				'Accept': 'application/vnd.api+json',
+				'Content-Type': 'application/vnd.api+json',
+				'Authorization': '={{$credentials.clicksign_access_token}}'
+			},
+		},
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Envelope',
+						value: 'envelopes',
+					}
+				],
+				default: 'envelopes',
+			},
+
+			...clicksignOperations,
+			...clicksignFields,
+		],
+	};
+}
